9415a99a183ce8c197161f2b6c4cd48dbffe3620
Marcos Vinicius A. Biasotto <marcos.biasotto@hotmail.com>
2021-07-07 17:52:58 -0300

    Projeto de testes criado

---
+++ Testes/TestesPessoa.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using CadastroCandidato;
+
+namespace Testes
+{
+    public class TestesPessoa
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Teste()
+        {
+
+        }
+    }
+}
